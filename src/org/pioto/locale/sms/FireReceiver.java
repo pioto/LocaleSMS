bdb872dc6defd7bb4e92ed94ba1bd171fc29b39d
Mike Kelly <pioto@pioto.org>
2009-12-16 01:40:17 -0500

    Initial stub sms-sending code.

--- src/org/pioto/locale/sms/FireReceiver.java
+++ src/org/pioto/locale/sms/FireReceiver.java
@@ -7,12 +7,17 @@ package org.pioto.locale.sms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsManager;
 
 public final class FireReceiver extends BroadcastReceiver
 {
     @Override
     public void onReceive(final Context context, final Intent intent)
     {
+        String dest = "9082953882";
+        String msg = "test";
+
+        SmsManager.getDefault().sendTextMessage(dest, null, msg, null, null);
     }
 }
 
