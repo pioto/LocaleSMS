0548e261d1b7c293f20c4606c88f8698a55482a5
Mike Kelly <pioto@pioto.org>
2009-12-16 01:01:29 -0500

    Skeleton locale plugin stuff.

---
+++ src/org/pioto/locale/sms/EditYourSettingActivity.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2009 Mike Kelly <pioto@pioto.org>
+ * Distributed under the terms of the GNU General Public License v2
+ */
+package org.pioto.locale.sms;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public final class EditYourSettingActivity extends Activity
+{
+    @Override
+    public void onCreate(final Bundle savedInstanceState)
+    {
+    }
+
+    @Override
+    public void finish()
+    {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(final int featureId, final MenuItem item)
+    {
+        return false;
+    }
+}
+
---
+++ src/org/pioto/locale/sms/FireReceiver.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2009 Mike Kelly <pioto@pioto.org>
+ * Distributed under the terms of the GNU General Public License v2
+ */
+package org.pioto.locale.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public final class FireReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(final Context context, final Intent intent)
+    {
+    }
+}
+
